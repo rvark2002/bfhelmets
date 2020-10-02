12273b6e61674abeb35117aa25121c8138cd8323
the4leaves <68232752+the4leaves@users.noreply.github.com>
2020-10-02 08:05:36 -0400

    test 2

--- src/com/vark/bfhelmet/Crafting.java
+++ src/com/vark/bfhelmet/Crafting.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
